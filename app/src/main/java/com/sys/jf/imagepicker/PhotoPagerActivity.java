ecc029b02d1fd1f4853c7a9c3177504ab7229bca
jianghuida <1192385871@qq.com>
2019-06-05 14:41:08 +0800

    更新20190605

--- app/src/main/java/com/sys/jf/imagepicker/PhotoPagerActivity.java
+++ app/src/main/java/com/sys/jf/imagepicker/PhotoPagerActivity.java
@@ -13,7 +13,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.List;
+
 import com.sys.jf.imagepicker.fragment.ImagePagerFragment;
 
 import com.sys.jf.imagepicker.R;
@@ -28,129 +32,107 @@ import static com.sys.jf.imagepicker.PhotoPreview.EXTRA_SHOW_DELETE;
  */
 public class PhotoPagerActivity extends AppCompatActivity {
 
-  private ImagePagerFragment pagerFragment;
+    private ImagePagerFragment pagerFragment;
+    private TextView photoNumberTv;
 
-  private ActionBar actionBar;
-  private boolean showDelete;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+        setContentView(R.layout.__picker_activity_photo_pager);
 
-    setContentView(R.layout.__picker_activity_photo_pager);
+        int currentItem = getIntent().getIntExtra(EXTRA_CURRENT_ITEM, 0);
+        List<String> paths = getIntent().getStringArrayListExtra(EXTRA_PHOTOS);
+        boolean showDelete = getIntent().getBooleanExtra(EXTRA_SHOW_DELETE, true);
 
-    int currentItem = getIntent().getIntExtra(EXTRA_CURRENT_ITEM, 0);
-    List<String> paths = getIntent().getStringArrayListExtra(EXTRA_PHOTOS);
-    showDelete = getIntent().getBooleanExtra(EXTRA_SHOW_DELETE, true);
-
-    if (pagerFragment == null) {
-      pagerFragment =
-          (ImagePagerFragment) getSupportFragmentManager().findFragmentById(R.id.photoPagerFragment);
-    }
-    pagerFragment.setPhotos(paths, currentItem);
-
-    Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(mToolbar);
-
-    actionBar = getSupportActionBar();
-
-    if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true);
-      updateActionBarTitle();
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        actionBar.setElevation(25);
-      }
-    }
-
-
-    pagerFragment.getViewPager().addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-      @Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        updateActionBarTitle();
-      }
-    });
-  }
-
-
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    if (showDelete){
-      getMenuInflater().inflate(R.menu.__picker_menu_preview, menu);
-    }
-    return true;
-  }
-
-
-  @Override public void onBackPressed() {
-    Intent intent = new Intent();
-    intent.putExtra(KEY_SELECTED_PHOTOS, pagerFragment.getPaths());
-    setResult(RESULT_OK, intent);
-    super.onBackPressed();
-  }
-
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-
-    if (item.getItemId() == android.R.id.home) {
-      onBackPressed();
-      return true;
-    }
-
-    if (item.getItemId() == R.id.delete) {
-      final int index = pagerFragment.getCurrentItem();
-
-      final String deletedPath =  pagerFragment.getPaths().get(index);
-
-      Snackbar snackbar = Snackbar.make(pagerFragment.getView(), R.string.__picker_deleted_a_photo,
-          Snackbar.LENGTH_LONG);
-
-      if (pagerFragment.getPaths().size() <= 1) {
-
-        // show confirm dialog
-        new AlertDialog.Builder(this)
-            .setTitle(R.string.__picker_confirm_to_delete)
-            .setPositiveButton(R.string.__picker_yes, new DialogInterface.OnClickListener() {
-              @Override public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-                pagerFragment.getPaths().remove(index);
-                pagerFragment.getViewPager().getAdapter().notifyDataSetChanged();
-                onBackPressed();
-              }
-            })
-            .setNegativeButton(R.string.__picker_cancel, new DialogInterface.OnClickListener() {
-              @Override public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-              }
-            })
-            .show();
-
-      } else {
-
-        snackbar.show();
-
-        pagerFragment.getPaths().remove(index);
-        pagerFragment.getViewPager().getAdapter().notifyDataSetChanged();
-      }
-
-      snackbar.setAction(R.string.__picker_undo, new View.OnClickListener() {
-        @Override public void onClick(View view) {
-          if (pagerFragment.getPaths().size() > 0) {
-            pagerFragment.getPaths().add(index, deletedPath);
-          } else {
-            pagerFragment.getPaths().add(deletedPath);
-          }
-          pagerFragment.getViewPager().getAdapter().notifyDataSetChanged();
-          pagerFragment.getViewPager().setCurrentItem(index, true);
+        if (pagerFragment == null) {
+            pagerFragment =
+                    (ImagePagerFragment) getSupportFragmentManager().findFragmentById(R.id.photoPagerFragment);
         }
-      });
+        pagerFragment.setPhotos(paths, currentItem);
 
-      return true;
+        photoNumberTv = findViewById(R.id.photo_number);
+        ImageView imgDestroyPage = findViewById(R.id.img_destory_page);
+        ImageView imgDeletePhoto = findViewById(R.id.img_delete_photo);
+        imgDestroyPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        imgDeletePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int index = pagerFragment.getCurrentItem();
+
+                final String deletedPath = pagerFragment.getPaths().get(index);
+
+                Snackbar snackbar = Snackbar.make(pagerFragment.getView(), R.string.__picker_deleted_a_photo,
+                        Snackbar.LENGTH_LONG);
+
+                if (pagerFragment.getPaths().size() <= 1) {
+
+                    // show confirm dialog
+                    new AlertDialog.Builder(PhotoPagerActivity.this)
+                            .setTitle(R.string.__picker_confirm_to_delete)
+                            .setPositiveButton(R.string.__picker_yes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                    pagerFragment.getPaths().remove(index);
+                                    pagerFragment.getViewPager().getAdapter().notifyDataSetChanged();
+                                    onBackPressed();
+                                }
+                            })
+                            .setNegativeButton(R.string.__picker_cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .show();
+
+                } else {
+
+                    snackbar.show();
+
+                    pagerFragment.getPaths().remove(index);
+                    pagerFragment.getViewPager().getAdapter().notifyDataSetChanged();
+                }
+
+                snackbar.setAction(R.string.__picker_undo, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (pagerFragment.getPaths().size() > 0) {
+                            pagerFragment.getPaths().add(index, deletedPath);
+                        } else {
+                            pagerFragment.getPaths().add(deletedPath);
+                        }
+                        pagerFragment.getViewPager().getAdapter().notifyDataSetChanged();
+                        pagerFragment.getViewPager().setCurrentItem(index, true);
+                    }
+                });
+            }
+        });
+
+        pagerFragment.getViewPager().addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                updateActionBarTitle();
+            }
+        });
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra(KEY_SELECTED_PHOTOS, pagerFragment.getPaths());
+        setResult(RESULT_OK, intent);
+        super.onBackPressed();
+    }
 
-  public void updateActionBarTitle() {
-    if (actionBar != null) actionBar.setTitle(
-        getString(R.string.__picker_image_index, pagerFragment.getViewPager().getCurrentItem() + 1,
-            pagerFragment.getPaths().size()));
-  }
+    public void updateActionBarTitle() {
+        photoNumberTv.setText(getString(R.string.__picker_image_index, pagerFragment.getViewPager().getCurrentItem() + 1,
+                pagerFragment.getPaths().size()));
+    }
 }
