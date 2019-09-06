aa2a549e040fa29479c2e33febf9361a716215e0
jianghuida <1192385871@qq.com>
2019-09-06 18:45:46 +0800

    2019-09-06

--- app/src/main/java/com/sys/jf/imagepicker/utils/FileUtils.java
+++ app/src/main/java/com/sys/jf/imagepicker/utils/FileUtils.java
@@ -7,8 +7,8 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.hjq.toast.ToastUtils;
 import com.sys.jf.imagepicker.R;
 
 import java.io.File;
@@ -20,8 +20,6 @@ import java.io.File;
  */
 public class FileUtils {
     public static int screenWidth = 0;
-    private static ToastHandler sToastHandler;
-    private static Toast sToast;
 
     public static boolean fileIsExists(String path) {
         if (path == null || path.trim().length() <= 0) {
@@ -76,50 +74,11 @@ public class FileUtils {
         tvToast.setRight(20);
         //设置文本
         tvToast.setText(text);
-        setView(view);
-        show(text);
+        ToastUtils.setView(view);
+        ToastUtils.show(text);
     }
 
     public static void CustomCenterToast(Context context, int resId) {
         CustomCenterToast(context, context.getString(resId), R.layout.layout_custom_toast);
-    }
-
-    public static void setView(View view) {
-
-        checkToastState();
-
-        // 这个 View 不能为空
-        if (view == null) {
-            throw new IllegalArgumentException("Views cannot be empty");
-        }
-
-        // 当前必须用 Application 的上下文创建的 View，否则可能会导致内存泄露
-        if (view.getContext() != view.getContext().getApplicationContext()) {
-            throw new IllegalArgumentException("The view must be initialized using the context of the application");
-        }
-
-        // 如果吐司已经创建，就重新初始化吐司
-        if (sToast != null) {
-            // 取消原有吐司的显示
-            sToast.cancel();
-            sToast.setView(view);
-        }
-    }
-
-    private static void checkToastState() {
-        // 吐司工具类还没有被初始化，必须要先调用init方法进行初始化
-        if (sToast == null) {
-            throw new IllegalStateException("ToastUtils has not been initialized");
-        }
-    }
-
-    public static void show(CharSequence text) {
-
-        checkToastState();
-
-        if (text == null || "".equals(text.toString())) return;
-
-        sToastHandler.add(text);
-        sToastHandler.show();
     }
 }
